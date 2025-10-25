4fb505a7ef2a411c6e899b736102f75c1bda4d4f
Lespedian ‎ <leandroramiro00@gmail.com>
2025-10-25 17:52:20 -0300

    primer commit

--- source/main.cpp
+++ source/main.cpp
@@ -1,131 +1,120 @@
 ﻿#ifndef NDEBUG
-#if defined(__has_include)
-#if __has_include(<vld.h>)
-#include <vld.h> // Visual Leak Detector solo si está instalado y en Debug
+#include <stdlib.h>
+#include <time.h>
+#include <vld.h>
+#include <stdbool.h>
 #endif
-#endif
-#endif
-
-#include <cstdlib>
-#include <ctime>
 #include "raylib.h"
+#include <utility>
+
+class Sprite {
+
+    public:
+
+    Texture2D* textura;
+    Vector2 posicion;
+    Vector2 velocidad;
+    float escala;
+    Color color;
+
+    Sprite(Texture2D* t = nullptr, Vector2 p = { 0,0 }, float s = 1.0f, Color c = WHITE, Vector2 v = { 200.0f })
+        : textura(t), posicion(p), escala(s), velocidad(v), color(c) {
+    }
+
+    void Draw() const {
+        if (textura && textura->id != 0) DrawTextureEx(*textura, posicion, 0.0f, escala, color);
+    }
+
+
+};
+
+
+
 
 int main(void)
 {
-    // Inicializamos una ventana de 1024x768 pixeles con un titulo personalizado
-    InitWindow(1024, 768, "Mi Proyecto! Leandro Sanchez");
+    // Inicializamos una ventana de 800x450 píxeles con un título personalizado
+    InitWindow(800, 450, "Paper Mario Sprite");
 
-    Color fondo = { 51, 143, 131, 255 };//color personalizado
-    Color texto = DARKPURPLE;
-
-    //Posición de la pelota en los ejes X e Y
-    Vector2 posicion = { 1024 / 2.0f, 768 / 2.0f };
-    //Velocidad que se le otorgará a la pelota
-    Vector2 velocidad = { 200.0f, 100.0f };
-
-    //Configura el framerate
+    // Configuramos el framerate deseado (opcional, pero recomendado)
     SetTargetFPS(60);
-    srand(time(NULL));
 
-    Color transparente = { 255, 0, 0, 120 };
-    Color colorPelota = Fade(DARKGREEN, 0.5f);
+    //Color fondo = { 110, 100, 215, 255 };//color violeta personalizado
+    //Color texto = DARKPURPLE;
 
-    int MensajeActual = 0;
+    
 
-    int rebotes = 0;
+    //Cargar textura desde archivos
+    Texture2D paperMario = LoadTexture("assets/papermario.png");
+    SetTextureFilter(paperMario, TEXTURE_FILTER_BILINEAR);
+    Texture2D paperMarioFondo = LoadTexture("assets/paperMarioFondo.png");
+    SetTextureFilter(paperMarioFondo, TEXTURE_FILTER_BILINEAR);
+
+    Vector2 posicionMario = { 210, 250 };
+    Vector2 posicionFondo = { 0, 0 };
+    Vector2 posicionMario2 = { 490, 150 };
+
+    Sprite fondo(&paperMarioFondo, { 0,0 }, 2.7f, WHITE);
+    Sprite MarioA(&paperMario, { 210,250 }, 1.0f, WHITE);
+    Sprite MarioB(&paperMario, { 490, 150 }, 1.0f, BLACK);
+
+    int activo = 0; // 0 -> a, 1 -> b
+    const float velocidad = 200.0f;
 
     // Bucle principal del juego (se repite hasta que se cierre la ventana)
     while (!WindowShouldClose())
     {
+
+        float deltaTime = GetFrameTime();
+
+        Sprite* SpriteActivo = (activo == 0) ? &MarioA : &MarioB;
+
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) SpriteActivo->posicion.x += velocidad * deltaTime;
+        if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) SpriteActivo->posicion.x -= velocidad * deltaTime;
+        if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) SpriteActivo->posicion.y -= velocidad * deltaTime;
+        if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) SpriteActivo->posicion.y += velocidad * deltaTime;
+
+        if (IsKeyPressed(KEY_SPACE)) {
+            std::swap(MarioA, MarioB);
+            std::swap(MarioA.color, MarioB.color);
+        }
+
+
+
+
+
+
+
         // Iniciamos la etapa de dibujo
         BeginDrawing();
 
         // Limpiamos la pantalla 
-        ClearBackground(fondo);
+        ClearBackground(BLACK);
 
+        //________Main code___________________________________________________
 
-        //_____________________________________
-         //Tiempo base
-        float deltaTime = GetFrameTime();
-     
-        //Para mostrar el contacto con algun borde
-        bool contacto = false;
+        //El drawing usando clases
+        fondo.Draw();
+        MarioA.Draw();
+        MarioB.Draw();
 
-        //Constantemente actualiza el tiempo y la velocidad
-        posicion.x += velocidad.x * deltaTime;
-        posicion.y += velocidad.y * deltaTime;
+        //Datos en pantalla
+        DrawText(TextFormat("Pos: %.1f , %.1f", SpriteActivo->posicion.x, SpriteActivo->posicion.y), 10, 10, 20, BLACK);
+        DrawText(TextFormat("Vel: %.1f , %.1f", SpriteActivo->velocidad.x, SpriteActivo->velocidad.y), 10, 30, 20, BLACK);
+        DrawText(TextFormat("Escala: %.2f", SpriteActivo->escala), 10, 55, 20, BLACK);
+        DrawText("Mover: Flechas / WASD   Swap: Tecla Espacio", 230, 420, 16, BLACK);
 
-        //Colisión izquierda y derecha
-        if ((posicion.x + 80) >= 1024) {
+ 
 
-            posicion.x = 1024 - 80;
-
-            velocidad.x *= -1;
-
-            rebotes = true;
-        }
-        else if ((posicion.x - 80) <= 0) {
-
-            posicion.x = 80;
-
-            velocidad.x *= -1;
-
-            rebotes = true;
-        }
-
-        //Colisión parte superior y parte inferior
-        if ((posicion.y + 80) >= 768) {
-
-            posicion.y = 768 - 80;
-
-            velocidad.y *= -1;
-
-            rebotes = true;
-        }
-        else if ((posicion.y - 80) <= 0) {
-
-            posicion.y = 80;
-
-            velocidad.y *= -1;
-
-            rebotes = true;
-        }
-
-        if (contacto) rebotes++;
-
-        //Para mostrar posición actual de la pelota
-        DrawText(TextFormat("Posicion: (%.1f, %.1f)", posicion.x, posicion.y), 10, 35, 20, RAYWHITE);
-
-        //Cantidad de rebotes
-        DrawText(TextFormat("Rebotes: %d", rebotes), 10, 60, 20, Fade(RAYWHITE, 0.6f));
-
-        //Dibuja un círculo, indicando posición, radio y color
-        DrawCircle(posicion.x, posicion.y, 80, (Color)colorPelota);
-
-
-        //___________________________
-
-
-		// Dibujamos un texto centrado, aca damos la posicion, el tamanio y el color
-        DrawText("BUENAS! MI NOMBRE ES LEAN!", 265, 75, 30, texto);
-
-        // Dibujamos un rectagulo a modo decorativo
-        DrawRectangle(190, 68, 650, 40, Fade(DARKGREEN, 0.5f)); // Fondo semitransparente para resaltar el texto
-
-        if (MensajeActual == 0) {
-            DrawText("Estoy programando en Raylib!", 265, 590, 30, texto);
-        }
-        else {
-            DrawText("Espero poder avanzar!", 320, 590, 30, texto);
-        }
-
-
+        //_________Fin del Main code_______________________________________________
 
         // Finalizamos el dibujo
         EndDrawing();
     }
 
     // Cerramos la ventana y liberamos recursos
+    UnloadTexture(paperMario);
+    UnloadTexture(paperMarioFondo);
     CloseWindow();
 
     return 0;
